4dfda747f528879fd061c313ba65ce1f248f90de
antoinepryy <antoine.ap.57@gmail.com>
2018-08-12 15:53:39 +0200

    First Commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: antoine
+ * Date: 12/08/2018
+ * Time: 15:45
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+}
