0ac88b2e57cc91309e24951e9157470e4176205e
antoinepryy <antoine.ap.57@gmail.com>
2018-08-24 22:39:13 +0200

    setup webpack & base twig

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,8 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+require('../css/accueil.css');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+
 // var $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
