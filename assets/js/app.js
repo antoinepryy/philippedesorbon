2fb451b52a74a7cb94a0f6dc98f9120c23e2160c
antoinepryy <antoine.ap.57@gmail.com>
2018-08-27 10:20:01 +0200

    MEP css et maison

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,40 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 require('../css/accueil.css');
+require('../css/maison.css');
+
+var path = window.location.pathname;
+switch (path) {
+    case '/LaMaison':
+        defineActive(path);
+        break;
+    case '/SavoirFaire':
+        defineActive(path);
+        break;
+    case '/Champagnes':
+        defineActive(path);
+        break;
+    case '/Boutique':
+        defineActive(path);
+        break;
+    case '/Vignoble':
+        defineActive(path);
+        break;
+    default:
+        console.log('Sorry, we are out of ' + expr + '.');
+}
 
 
+// function lowFooter() {
+//     document.getElementById("footer").style.position= "absolute";
+//     document.getElementById("footer").style.bottom= "0";
+//     document.getElementById("footer").style.left= "0";
+//     document.getElementById("footer").style.right= "0";
+// }
+
+function defineActive(element){
+    element = element.substr(1);
+    document.getElementById(element).className = "active";
+}
 // var $ = require('jquery');
 
