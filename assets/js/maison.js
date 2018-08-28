a8bf4861817eca4502ccd64c1d2cf9aff3be69e2
antoinepryy <antoine.ap.57@gmail.com>
2018-08-28 16:29:08 +0200

    autocroll

--- assets/js/maison.js
+++ assets/js/maison.js
@@ -1,12 +1,13 @@
 const $ = require('jquery');
 
+
 $('a[href^="#"]').click(function(){
     var the_id = $(this).attr("href");
     if (the_id === '#') {
         return;
     }
     $('html, body').animate({
-        scrollTop:$(the_id+"-delay").offset().top
+        scrollTop:$(the_id).offset().top
     }, 'slow');
     return false;
 });
