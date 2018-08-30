3a7cca8afd53704338fb628f5a0c1c6e25e422e6
antoinepryy <antoine.ap.57@gmail.com>
2018-08-31 01:28:23 +0200

    add champagne works

--- src/Entity/Champagne.php
+++ src/Entity/Champagne.php
@@ -3,12 +3,9 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\HttpFoundation\File\File;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ChampagneRepository")
- * @Vich\Uploadable
  */
 class Champagne
 {
@@ -79,23 +76,6 @@ class Champagne
      */
     private $presentation_4;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     * @var string
-     */
-    private $image;
-
-    /**
-     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
-     * @var File
-     */
-    private $imageFile;
-
-    /**
-     * @ORM\Column(type="datetime")
-     * @var \DateTime
-     */
-    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -244,34 +224,5 @@ class Champagne
         $this->presentation_4 = $presentation_4;
 
         return $this;
-    }
-
-
-    public function setImageFile(File $image = null)
-    {
-        $this->imageFile = $image;
-
-        // VERY IMPORTANT:
-        // It is required that at least one field changes if you are using Doctrine,
-        // otherwise the event listeners won't be called and the file is lost
-        if ($image) {
-            // if 'updatedAt' is not defined in your entity, use another property
-            $this->updatedAt = new \DateTime('now');
-        }
-    }
-
-    public function getImageFile()
-    {
-        return $this->imageFile;
-    }
-
-    public function setImage($image)
-    {
-        $this->image = $image;
-    }
-
-    public function getImage()
-    {
-        return $this->image;
     }
 }
