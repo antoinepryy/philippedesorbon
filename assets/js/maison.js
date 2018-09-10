3bac5b716b7189902a942ed0bc0a679d61993ebd
Antoine Perry <antoine.ap.57@gmail.com>
2018-09-10 23:11:19 +0200

    anim

--- assets/js/maison.js
+++ assets/js/maison.js
@@ -14,9 +14,16 @@ $('a[href^="#"]').click(function(){
 
 $('.circle').click(function(){
     var indexAnim = $(this).attr('id').substr(7);
-    $('#anim-picture').attr('src','/ressource/image/maison/slide-'+ indexAnim +'.jpg');
+    $('#anim-picture').fadeOut();
+    $('#anim-picture').fadeOut('slow');
+    $('#anim-picture').promise().done(function(){
+        $('#anim-picture').attr('src','/ressource/image/maison/slide-'+ indexAnim +'.jpg');
+    }).promise().done(function() {
+        $('#anim-picture').fadeIn('slow');
+    });
     $('.circle').removeClass('circle-active');
     $('.anim-txt').css('display', 'none');
     $('#anim-txt-'+indexAnim).css('display', 'block');
+    $('#anim-txt-'+indexAnim).addClass('fadein');
     $(this).addClass('circle-active');
 });
