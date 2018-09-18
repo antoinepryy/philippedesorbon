b09e0b81a7c86c5bb8b5f5952fde7e626db8b85f
antoinepryy <antoine.ap.57@gmail.com>
2018-09-19 00:06:08 +0200

    controller shop

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: antoine
+ * Date: 19/09/2018
+ * Time: 00:00
+ */
+
+namespace App\Controller;
+
+
+class ShopController
+{
+
+    public function addProduct(){
+
+    }
+
+    public function removeOneProduct(){
+
+    }
+
+    public function removeAllProduct(){
+
+    }
+
+}
