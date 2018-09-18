ce61aad4586b95e650d752947d448b58c139c387
antoinepryy <antoine.ap.57@gmail.com>
2018-09-18 10:25:07 +0200

    locale + terms of use checkbox

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -16,6 +16,8 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class UserType extends AbstractType
 {
@@ -33,6 +35,11 @@ class UserType extends AbstractType
                 'type' => PasswordType::class,
                 'first_options'  => array('label' => 'Password'),
                 'second_options' => array('label' => 'Confirmer mot de passe'),
+            ))
+            ->add('termsAccepted', CheckboxType::class, array(
+                'mapped' => false,
+                'constraints' => new IsTrue(),
+                'label' => 'J\'accepte les termes d\'utilisation du site',
             ))
         ;
     }
