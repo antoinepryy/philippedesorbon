c1fc6f4c23b4630048181897a67042fbf61ffa82
antoinepryy <antoine.ap.57@gmail.com>
2018-09-19 11:10:44 +0200

    add clear cart

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -19,21 +19,31 @@ class ShopController extends Controller
     /**
      * @Route("/AddProduct", name="add_product")
      */
-    public function addProduct(SessionInterface $session){
+    public function addProduct(SessionInterface $session, $id){
         $session->set('test',2);
         return $this->render('view/accueil.html.twig');
     }
+
     /**
      * @Route("/RemoveOneProduct", name="remove_one_product")
      */
-    public function removeOneProduct(SessionInterface $session){
+    public function removeOneProduct(SessionInterface $session, $id){
         $foobar = $session->get('test');
         die(var_dump($foobar));
         return $this->render('view/accueil.html.twig');
 
     }
-
+    /**
+     * @Route("/RemoveAllProducts", name="remove_all_products")
+     */
     public function removeAllProduct(){
+
+    }
+
+    /**
+     * @Route("/ClearCart", name="clear_cart")
+     */
+    public function clearCart(){
 
     }
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -93,9 +93,6 @@ class User implements UserInterface
 
 
 
-
-
-
     /**
      * @return mixed
      */
