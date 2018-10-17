77ec31248e73cd0c0ea4a340c7597bd2ede6d7a0
Antoine Perry <antoine.ap.57@gmail.com>
2018-10-17 22:13:58 +0200

    forgot pass test

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,11 +9,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -51,7 +54,6 @@ class SecurityController extends AbstractController
         } else {
             $cartSize = 0;
         }
-
         $defaultData = [];
         $form = $this->createFormBuilder($defaultData)
             ->add('email', TextType::class,[
@@ -71,17 +73,28 @@ class SecurityController extends AbstractController
             ]);
             if ($foundUser != null){
 
-                $messageClient = (new \Swift_Message('Philippe de Sorbon'))
+                $generatedHash = sha1(uniqid());
+                $foundUser->setPasswordLink($generatedHash);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($foundUser);
+                $entityManager->flush();
+
+                $messageClient = (new \Swift_Message('Récupération de mot de passe'))
                     ->setFrom('philippedesorbon@gmail.com')
                     ->setTo($foundUser->getEmail())
                     ->setBody(
                         $this->renderView(
-                            'emails/passwordLink.html.twig'
+                            'emails/passwordLink.html.twig',[
+                                'hashCode' => $generatedHash
+                            ]
                         ),
                         'text/html'
                     );
 
                 $mailer->send($messageClient);
+                return $this->render('view/accueil.html.twig', [
+                    'cartSize' => $cartSize
+                ]);
 
 
             }
@@ -98,5 +111,64 @@ class SecurityController extends AbstractController
             'cartSize' => $cartSize,
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/RecupererMotDePasse/{hashCode}", name="recover_password")
+     */
+    public function recoverPassword(SessionInterface $session, Request $request, UserPasswordEncoderInterface $passwordEncoder, $hashCode){
+        if ($session->has('cart')) {
+            $cartSize = count($session->get('cart'));
+        } else {
+            $cartSize = 0;
+        }
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $foundUser = $repository->findOneBy([
+            'passwordLink' => $hashCode
+        ]);
+        if ($foundUser != null ){
+            $form = $this->createFormBuilder($foundUser)
+                ->add('plainPassword', RepeatedType::class, array(
+                    'type' => PasswordType::class,
+                    'first_options'  => array('label' => 'Mot de passe', 'attr'=>['placeholder'=>'Mot de passe']),
+                    'second_options' => array('label' => 'Confirmer mot de passe', 'attr'=>['placeholder'=>'Confirmer mot de passe']),
+                ))
+                ->add('Valider', SubmitType::class)
+                ->getForm();
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $password = $passwordEncoder->encodePassword($foundUser, $foundUser->getPlainPassword());
+                $foundUser->setPassword($password);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($foundUser);
+                $entityManager->flush();
+
+                return $this->render('view/accueil.html.twig', [
+                    'cartSize' => $cartSize,
+                    'form' => $form->createView(),
+                ]);
+            }
+
+            return $this->render('security/forgotPassword.html.twig', [
+                'cartSize' => $cartSize,
+                'form' => $form->createView(),
+            ]);
+
+
+        }
+        else{
+            throw $this->createNotFoundException('Code non valide');
+        }
+
+
+    }
+
+    /**
+     * @Route("/ChangementMotDePasse/{hashCode}", name="change_password")
+     */
+    public function changePassword(SessionInterface $session, Request $request, $hashCode = "" ){
+
     }
 }
